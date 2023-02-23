d3ffe2d574e68d3b64512b4abf9fb6536d4a695c
Matthew Almano <matthew.almano51@gmail.com>
2023-02-23 16:42:20 +0200

    [ADDED] Cors middleware to express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
 import connect from "./clients/mongoose-client";
 import WeightRouter from "./routes/weight-routes";
 
 const startApp = async () => {
   await connect();
   const app: Express = express();
-  const API_PORT = 3000;
+  const API_PORT = 8000;
 
   app.use(express.json());
+  app.use(cors());
   app.use(WeightRouter);
 
   app.listen(API_PORT, () => console.log(`Server running on port ${API_PORT}`));
