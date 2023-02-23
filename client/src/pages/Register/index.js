5b43e3b047dd0db5b2e324466489440a61912ec5
Matthew Almano <matthew.almano51@gmail.com>
2023-02-23 23:30:10 +0200

    [REFACTOR] Code cleanup

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
 import { Form, FormButton, FormInput, FormLabel, FormText } from '../../components/common/FormElements';
